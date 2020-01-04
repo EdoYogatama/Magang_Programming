adec435b06b18e8bb023a7232621e1ada7a5207b
Funny Mickey <47658064+EdoYogatama@users.noreply.github.com>
2020-01-04 20:34:38 +0700

    Simple Go To
    Run simple go to SITL with mission planner success

--- SimpleGoTo.py
+++ SimpleGoTo.py
@@ -1,7 +1,9 @@
-from dronekit import connect
+from dronekit import connect, VehicleMode, LocationGlobalRelative
 import argparse as ap
 import time
 import waypoints as wp
+import future
+import math
 
 # Argument Parser, command to connect to the vehicle
 parser  = ap.ArgumentParser(description='Control Copter and send commend in GUIDED mode')
@@ -11,6 +13,8 @@ args    = parser.parse_args()
 
 connection_string   = args.connect
 sitl                = None
+
+waypoints           = 0
 
 # If connection_string not specified import sitl and start using sitl
 if not connection_string:
@@ -50,7 +54,7 @@ def arm_and_tekoff(altitude_target):
     while True:
         current_altitude = vehicle.location.global_relative_frame.alt
         print("Going up ",current_altitude)
-        if current_altitude >= aTargetAltitude * 0.85:
+        if current_altitude >= altitude_target * 0.85:
             print("Reached target altitude")
             break
         time.sleep(1)
@@ -60,15 +64,21 @@ def getWP(lat_home = 0.0, lon_home = 0.0, alt_home = 0.0):
     lat_target = []
     lon_target = []
 
-    for i in range(len(wp.gets)):
+    waypoints  = len(wp.gets())    
+    for i in range(waypoints):
         if i == 0:
             lat_home    = wp.get(i)[0]
             lon_home    = wp.get(i)[1]
         else:
             lat_target.append(wp.get(i)[0])
-            lon_target.append(wp.get(i)[0])
+            lon_target.append(wp.get(i)[1])
 
     return lat_home,lon_home,lat_target,lon_target
+
+def get_distance_metres(aLocation1, aLocation2):
+    dlat        = aLocation2.lat - aLocation1.lat
+    dlong       = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
 def main():
     altitude_target     = 2.0
@@ -81,24 +91,46 @@ def main():
     arm_and_tekoff(altitude_target)
     vehicle.flush()
     time.sleep(1)
-
+    
     lat_home,lon_home,lat_target,lon_target = getWP()
     home    = LocationGlobalRelative(lat_home,lon_home,vehicle.location.global_relative_frame.alt)
 
     try:
-        for i in range(len(waypoints)):
+        for i in range(len(lat_target)):
 
+            currentlat = vehicle.location.global_relative_frame.lat
+            currentlon = vehicle.location.global_relative_frame.lon
+            
             location_target = LocationGlobalRelative(lat_target[i],lon_target[i],vehicle.location.global_relative_frame.alt)
-            print('goto ' + location_target)
+            
+            print('goto ' + str(location_target))
+            
             vehicle.simple_goto(location_target)
-        
+            #time.sleep(30)
+            
+            while vehicle.mode == VehicleMode("GUIDED"):
+                remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, location_target)
+                print(remainingDistance)
+                if remainingDistance <= 5: #Just below target, in case of undershoot.
+                    print ("Reached target")
+                    print("Drop")
+                    break
+                time.sleep(0.1)
+                    
         vehicle.mode = VehicleMode('RTL')
 
     except KeyboardInterrupt:
+        vehicle.mode = VehicleMode('RTL')
         print('ERROR')
+    except SyntaxError:
+        vehicle.mode = VehicleMode('RTL')
+        print("There is error with your syntax, please check it again")
 
     if sitl:
         sitl.stop
 
     vehicle.flush()
-    vehicle.close() 
+    vehicle.close()
+
+if __name__ == "__main__":
+    main()
