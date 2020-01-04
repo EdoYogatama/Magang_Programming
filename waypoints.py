72ca8dbafec3678127d4371a9d3f42f2d995de29
Funny Mickey <47658064+EdoYogatama@users.noreply.github.com>
2020-01-04 20:37:20 +0700

    waypoint loader

--- waypoints.py
+++ waypoints.py
@@ -2,11 +2,9 @@ import numpy as np
 
 def setup():
 	global waypoints
-	
-	waypoints = np.loadtxt("vtol.waypoints", dtype=float, skiprows=1, usecols=(8,9))
-	
+	waypoints = np.loadtxt("bayucaraka.waypoints", dtype=float, skiprows=1, usecols=(8,9))
 def gets():
 	return waypoints
 	
 def get( index ):
-	return waypoints[ index ]
+        return waypoints[ index ]
